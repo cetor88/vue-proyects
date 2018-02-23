0accd54b0f5f2bf699c65b501158bf794e9ddac7
Hector Hernandez Tetlalmatzi <hector.hernandez@gnp.com.mx>
2018-02-22 20:04:32 -0600

    se agrega tabla adeudo por persona.

--- cepcm_movil_admin/src/components/home/notificacionAdeudoPersona.vue
+++ cepcm_movil_admin/src/components/home/notificacionAdeudoPersona.vue
@@ -39,6 +39,34 @@
             </v-flex>
           
         </v-layout>
+        <v-layout>
+            <v-data-table
+                v-bind:headers="headers"
+                :items="items"
+                class="elevation-1"
+            >
+                <template slot="headerCell" slot-scope="props">
+                    <v-tooltip bottom>
+                    <span slot="activator">
+                        {{ props.header.text }}
+                    </span>
+                    <span>
+                        {{ props.header.text }}
+                    </span>
+                    </v-tooltip>
+                </template>
+                <template slot="items" slot-scope="props">
+                    <td>{{ props.item.name }}</td>
+                    <td class="text-xs-right">{{ props.item.calories }}</td>
+                    <td class="text-xs-right">{{ props.item.fat }}</td>
+                    <td class="text-xs-right">{{ props.item.carbs }}</td>
+                    <td class="text-xs-right">{{ props.item.protein }}</td>
+                    <td class="text-xs-right">{{ props.item.sodium }}</td>
+                    <td class="text-xs-right">{{ props.item.calcium }}</td>
+                    <td class="text-xs-right">{{ props.item.iron }}</td>
+                </template>
+            </v-data-table>
+            </v-layout>
     </v-form>
     <v-flex>
 
